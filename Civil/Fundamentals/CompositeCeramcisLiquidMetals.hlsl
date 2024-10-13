3bea068551aef0235fb0955288fceef9552a7511
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-13 12:33:02 +0200

    Create CompositeCeramcisLiquidMetals.hlsl

---
+++ Civil/Fundamentals/CompositeCeramcisLiquidMetals.hlsl
@@ -0,0 +1,6 @@
+
+//The term liquid metals seems to be far away from reality. But this is not the case
+//because, as I expressed my futur preview physics researches, functions or equations at that step
+//can be reduced and we don't need to transform log(x) as a constant. This propagate some sort of
+//Signals between fundamentals of terra models to maximum entropy models ! Maximum entropy models
+//themselves...... (I have to do my own stuffs here !!! Please wait !!!)
