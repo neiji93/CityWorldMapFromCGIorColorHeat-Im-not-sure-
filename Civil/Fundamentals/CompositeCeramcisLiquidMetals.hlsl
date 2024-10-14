42c9c5f8ab389763c5285ea6766ff1e9893aa37f
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-14 10:12:52 +0200

    Update CompositeCeramcisLiquidMetals.hlsl

--- Civil/Fundamentals/CompositeCeramcisLiquidMetals.hlsl
+++ Civil/Fundamentals/CompositeCeramcisLiquidMetals.hlsl
@@ -4,3 +4,6 @@
 //can be reduced and we don't need to transform log(x) as a constant. This propagate some sort of
 //Signals between fundamentals of terra models to maximum entropy models ! Maximum entropy models
 //themselves...... (I have to do my own stuffs here !!! Please wait !!!)
+
+
+void terraIsolate....
