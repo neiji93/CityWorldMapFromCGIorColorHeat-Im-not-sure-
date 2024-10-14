6b8316a5864b5b27cfe2dbbf8e2d179e4bb197cb
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-14 10:47:38 +0200

    Update CompositeCeramcisLiquidMetals.hlsl

--- Civil/Fundamentals/CompositeCeramcisLiquidMetals.hlsl
+++ Civil/Fundamentals/CompositeCeramcisLiquidMetals.hlsl
@@ -6,4 +6,21 @@
 //themselves...... (I have to do my own stuffs here !!! Please wait !!!)
 
 
-void terraIsolate....
+
+//tested over time.
+//This function checks relationships between other ....
+//return texture array to sample over time as we previously in CG field prevented to take
+//all the textures in BSDF calculus. Then, we need differential methods !
+//This seems gracefully not a CG function !
+//Then, the random corpus will be diffeomorphisms. And in physics, diffeomorphisms
+//can occur in high school formulas. So, just return if the  parameters contain high school formulas.
+void terraIsolateMathRandomCorpus( .... )
+{
+  //It is not safe nor unsafe to use diffeormorphisms but we have to prevent using computer graphics knowledges !
+
+
+  //...
+}
+
+
+
