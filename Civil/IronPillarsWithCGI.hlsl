be6d0032ba8dc73ba59d157a0c9b8deef6b3234d
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-14 17:12:48 +0100

    Update IronPillarsWithCGI.hlsl

--- Civil/IronPillarsWithCGI.hlsl
+++ Civil/IronPillarsWithCGI.hlsl
@@ -2,3 +2,28 @@
 //as we need cosinus x, we can use bsdf
 void restoreAlbedoFromIronBsdf(...)
 {}
+
+
+//if we use physics, there may be a way to use scrhodinger equations without
+//solving them with RK4 method. Then, it results that there must be a better
+//method than the previous method if we use formal series to generate randomness.
+//This can be true if we don't use RK4 again here!
+//see: https://github.com/navya19j/Solving-the-Schrodinger-Equation.git
+//Noticec that this can be used in computer graphics and some other functiond
+//can be created from only those comments. See next lines.
+void secureRestoreAlbedoFromIronBSDF()
+{
+  //we don t have to use heat transfer as we have two suggestions of RK4
+}
+
+//a way to use computer vision. But this is basics !
+//see computer vision books that deal with restoring full scene.
+void einsumRK4ForDiffuseFullScene()
+{
+
+}
+
+//computer vision again. But in my repositories, I tried a little bit to use
+//diffuse component several times (severalCGISoftwares).
+//So here, to cancel diffuse component below, we have to use a model
+//void 
